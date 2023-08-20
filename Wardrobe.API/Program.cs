c549fe1877b9585d2d406b9f7b8f8b7a53032833
Luiz Paulo <lindennerd@gmail.com>
2023-08-20 19:17:38 -0300

    moved to static program format

--- Wardrobe.API/Program.cs
+++ Wardrobe.API/Program.cs
@@ -12,124 +12,131 @@ using Wardrobe.Application.User;
 using Wardrobe.CrossCutting.Registrations;
 using Wardrobe.Infra.Database;
 using Wardrobe.Infra.Database.Cloth;
-using Wardrobe.Infra.HttpClients;
 
-var builder = WebApplication
-    .CreateBuilder(args);
+namespace Wardrobe.API;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(option =>
+internal static class Program
 {
-    option.SwaggerDoc("v1", new OpenApiInfo { Title = $"Wardrobe API - {builder.Environment.EnvironmentName}", Version = "v1" });
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    public static void Main(string[] args)
     {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+        var builder = WebApplication
+            .CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(option =>
         {
-            new OpenApiSecurityScheme
+            option.SwaggerDoc("v1", new OpenApiInfo { Title = $"Wardrobe API - {builder.Environment.EnvironmentName}", Version = "v1" });
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
-                Reference = new OpenApiReference
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
                 {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
                 }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
+            });
+        });
 
-builder.Services.Configure<MongoConnectionSettings>(
-    builder.Configuration.GetSection("MongoDB")
-);
-builder.Services.UseAzure(builder.Configuration);
+        builder.Services.Configure<MongoConnectionSettings>(
+            builder.Configuration.GetSection("MongoDB")
+        );
+        builder.Services.UseAzure(builder.Configuration);
 
-builder.Services.AddScoped<IClothesRepository, ClothesRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IIdentityRepository, IdentityRepository>();
-builder.Services.AddScoped<IBackgroundRemovalQueueService, BackgroundRemovalQueueService>();
-builder.Services.AddScoped<ISecurityService, SecurityService>();
-builder.Services.AddScoped<IClothService, ClothService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IUploadImageService, UploadImageService>();
+        builder.Services.AddScoped<IClothesRepository, ClothesRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IIdentityRepository, IdentityRepository>();
+        builder.Services.AddScoped<IBackgroundRemovalQueueService, BackgroundRemovalQueueService>();
+        builder.Services.AddScoped<ISecurityService, SecurityService>();
+        builder.Services.AddScoped<IClothService, ClothService>();
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<IUploadImageService, UploadImageService>();
 
-builder.Services.AddMemoryCache();
+        builder.Services.AddMemoryCache();
 
-builder.Services.UseGeolocationHttpClient(builder.Configuration);
-builder.Services.UseWeatherHttpClient(builder.Configuration);
+        builder.Services.UseGeolocationHttpClient(builder.Configuration);
+        builder.Services.UseWeatherHttpClient(builder.Configuration);
 
-var tokenConfig = builder.Configuration.GetSection("TokenConfiguration");
-builder.Services.AddScoped<ITokenService, TokenService>(opt => new TokenService(new TokenConfiguration
-{
-    Secret = tokenConfig["Secret"],
-    ExpirationHours = int.Parse(tokenConfig["Expiration"])
-}));
+        var tokenConfig = builder.Configuration.GetSection("TokenConfiguration");
+        builder.Services.AddScoped<ITokenService, TokenService>(opt => new TokenService(new TokenConfiguration
+        {
+            Secret = tokenConfig["Secret"],
+            ExpirationHours = int.Parse(tokenConfig["Expiration"])
+        }));
 
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenConfig["Secret"])),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+        builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenConfig["Secret"])),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
 
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq((context, cfg) =>
-    {
-        var rabbitMqConfiguration = builder.Configuration.GetSection("RabbitMQ");
+        builder.Services.AddMassTransit(x =>
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                var rabbitMqConfiguration = builder.Configuration.GetSection("RabbitMQ");
         
-        cfg.Host(rabbitMqConfiguration["Host"]);
-        cfg.ConfigureEndpoints(context);
-    });
-});
+                cfg.Host(rabbitMqConfiguration["Host"]);
+                cfg.ConfigureEndpoints(context);
+            });
+        });
 
-builder.Services.AddCors( options =>
-{
-    options.AddPolicy("AllowAll", corsBuilder =>
-    {
-        corsBuilder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
+        builder.Services.AddCors( options =>
+        {
+            options.AddPolicy("AllowAll", corsBuilder =>
+            {
+                corsBuilder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
 
-builder.Services.AddLogging(logging =>
-{
-    logging.ClearProviders();
-    logging.AddNLog(builder.Configuration.GetSection("Logging"));
-});
+        builder.Services.AddLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddNLog(builder.Configuration.GetSection("Logging"));
+        });
 
 
 
-var app = builder.Build();
+        var app = builder.Build();
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
-app.UseCors();
+        app.UseCors();
 
-app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-app.UseAuthorization();
+        app.UseAuthorization();
 
-app.MapControllers();
+        app.MapControllers();
 
-app.Run();
+        app.Run();
+    }
+}
